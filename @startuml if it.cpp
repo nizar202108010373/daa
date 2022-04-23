12ce272a81c3fa5fd17153846e1fbf465a591a58
nizar202108010373 <103300963+nizar202108010373@users.noreply.github.com>
2022-04-23 11:45:29 +0700

    Add files via upload

---
+++ @startuml if it.cpp
@@ -0,0 +1,12 @@
+@startuml if
+    start
+    if (int a lebih besar dari) the (yes)
+    #lightskyblue:cout << a lebih besar dari b;
+    else (no)
+    #lightskyblue:cout << b lebih kecil dari a;
+    endif
+    stop
+
+
+@enduml
+
